07e040873a210d70957b3b5ab2b1a78bb3da6d9c
Wisegith <moniwise50@gmail.com>
2024-10-26 23:59:55 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let input;
+
+document.getElementById("userSubmit").onclick = function(){
+    input = document.getElementById("userFullName").value
+    console.log(input);
+
+    var input = document.getElementById("userEmail").value
+    console.log(input);
+
+    var input = document.getElementById("userSubject").value
+    console.log(input);
+}
